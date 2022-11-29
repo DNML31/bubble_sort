529fe06633ac62a48295f571836962e60f51c6d9
DNML31 <danielchun31@gmail.com>
2022-11-29 10:33:03 -0800

    Sweep and check methods
    This commit changes the sweep method and adds a check method.
    
    The sweep method now compares [i-1] > [i] instead of [i] > [i+1]. This
    may have caused the return array to have an extra nil when trying to
    sort the test array.
    
    The check method is meant to check the elements in the array after the
    intial sweep. If an element, i, is less than i-1, it will call the sweep
    method again, else it will just break.
    
    Currently the return array isn't fully sorted in order; the only issue
    is in the first two elements. [2, 0, 2, 3, 4, 78]

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,72 +1,45 @@
-# [4,3,78,2,0,2]
 def bubble_sort(array)
-  # how_many = array.length
 
-  # p how_many
-  # how many elements
+  # compare an element in the array to the one right after it.
+  # if they are out of order then swap them.
+  # do this for every element in the array.
 
-  # p array[how_many - 1]
-  # the last element
-
-  # swap elements within the array
-  # array[0], array[1] = array[1], array[0]
-  
   def sweep(array)
-    i = 0
-    while i < array.length - 1
-      if array[i] > array[i + 1]
-        array[i] , array [i + 1] = array[i + 1] , array[i]
+    for i in (0..array.length - 1)
+      if array[i-1] > array[i].to_i
+        array[i-1],array[i] = array[i],array[i-1]
       end
-      i += 1
     end
   end
 
-  if array[0] < array[1] && array[1] < array[2]
-    p array
-  else
-    sweep(array)
-    p array
-  end 
-
-
+  
+  def check(array)
+    
+    for i in (0..array.length - 1)
+      if array[i-1] > array[i]
+        sweep(array)
+      else
+        break
+      end
+    end
+    
+  end
+  
+  
+  sweep(array)
+  check(array)
+  
+  p array
 end
 
-  # def sweep(array)
-  #   first_el = array[0]
-  #   second_el = array[1]
-  #   third_el = n - 2? OR array[2]?
-  #   last_el = n - 1
+  # for i in (0..(array.length - 1))
+  #   if array[i] > array [i+1]
+  #     array[i],array[i+1] = array[i+1],array[i]
+  #   elsif array[i] < array [i+1]
+  #     p array
+  #   end 
+  # end
 
-  # how many elements? the number of comparisons in one sweep is (n-1).
+# end
 
-  # *****
-  # example there is 5 elements (so 4 comparisons in one sweep)
-  # array = [1,2,3,4,5]
-
-  # last = array.length (this returns how many elements)
-  # array[last - 1] (this returns the last element)
-
-  # first two elements are easy to find their indexes
-  # if array[0] > array[1] then array[0], array[1] = array[1], array[0]
-
-  # what about in between elements' indexes?
-  # if array[1] > array[2] then ....
-  # if array[2] > array[3] then ....
-
-  # last two elements are easy to find their indexes
-  # if array["second to last"] > array[last - 1] then ....
-      
-
-
-
-
-
-
-
-bubble_sort([3,2,0])
-# bubble_sort([4,3,78,2,0,2])
-# Build a method #bubble_sort that takes an array and returns a sorted 
-# array. It must use the bubble sort methodology (using #sort would be pretty 
-# pointless, wouldn’t it?).
-
-# should result in [0,2,2,3,4,78]
+bubble_sort([4,3,78,2,0,2])
