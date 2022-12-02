5fccc9dfa98ea9c52388a5bab9bb21ad5225942d
DNML31 <danielchun31@gmail.com>
2022-12-02 14:33:45 -0800

    Had to check solutions to complete...
    This assignment was difficult and I resorted to looking at another's
    code to understand how to solve it. Rather than just simply copying the
    code, I tried to understand line-by-line what was happening and tried to
    reproduce it again. There were still some parts I got wrong and was
    unable to reproduce the same results.
    
    The solution that I saw used a while loop that was embedded in another
    while loop. The innermost while loop would keep looping until 'i', the
    last element in the array is put through the comparison if statement
    (i is increased by 1 after each if block). After each if statement is
    run, the 'unsorted' variable is assigned 'true'.
    
    Once the code is out of the inner while loop, the outer while loop is
    conditioned to keep running while 'unsorted' is true. Since 'unsorted'
    has been assigned true, the next two lines that are run change the value
    of i to 0 and 'unsorted' to false (which means the outer while loop's
    condition is no longer met). By now the array is sorted and printed.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,45 +1,22 @@
 def bubble_sort(array)
 
-  # compare an element in the array to the one right after it.
-  # if they are out of order then swap them.
-  # do this for every element in the array.
+  unsorted = true
+  while unsorted do
 
-  def sweep(array)
-    for i in (0..array.length - 1)
-      if array[i-1] > array[i].to_i
-        array[i-1],array[i] = array[i],array[i-1]
+    i = 0
+    unsorted = false
+
+    while i < (array.length - 1)
+
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        unsorted = true
       end
+
+      i += 1
     end
   end
-
-  
-  def check(array)
-    
-    for i in (0..array.length - 1)
-      if array[i-1] > array[i]
-        sweep(array)
-      else
-        break
-      end
-    end
-    
-  end
-  
-  
-  sweep(array)
-  check(array)
-  
   p array
 end
-
-  # for i in (0..(array.length - 1))
-  #   if array[i] > array [i+1]
-  #     array[i],array[i+1] = array[i+1],array[i]
-  #   elsif array[i] < array [i+1]
-  #     p array
-  #   end 
-  # end
-
-# end
 
 bubble_sort([4,3,78,2,0,2])
